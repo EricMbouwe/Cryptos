19ae8b581c79afddb51e656f93a896445805ea7e
ericmbouwe <ericmbouwe@yahoo.fr>
2021-03-04 21:46:27 +0100

    add copyright textinto the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Footer = () => (
   <div className="footer">
-    <div className="container">
+    <div className="container flex flex-ai-c flex-jc-sb">
       <a href="https://nomics.com" target="blank">
         Crypto Market Cap & Pricing Data Provided By Nomics
       </a>
+      <span>&copy;2021 Le Consultant</span>
     </div>
   </div>
 );
